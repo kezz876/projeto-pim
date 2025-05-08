9cd2f18d405251c95324994c5975cf3b51437096
Lima <106927164+kezz876@users.noreply.github.com>
2025-05-08 18:40:24 -0300

    adm/obj/debug/model

---
+++ build/codes/model/Emprstimo.cs
@@ -0,0 +1,57 @@
+namespace PIM_3.model
+{
+    public class Emprestimo
+    {
+        public string ISBN { get; set; }
+        public int CpfLeitor { get; set; }
+        public DateTime Data { get; set; }
+
+        public bool EscolherELancarEmprestimo(List<Livros> listaLivros)
+        {
+            var livrosDisponiveis = listaLivros.Where(l => l.Disponivel).ToList();
+
+            if (livrosDisponiveis.Count == 0)
+            {
+                Console.WriteLine("Nenhum livro disponível para empréstimo.");
+                return false;
+            }
+
+            Console.WriteLine("Livros disponíveis para empréstimo:");
+            for (int i = 0; i < livrosDisponiveis.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {livrosDisponiveis[i].NomeLivro} - {livrosDisponiveis[i].Autor}");
+            }
+
+            Console.Write("Digite o número do livro que deseja emprestar: ");
+            if (int.TryParse(Console.ReadLine(), out int opcao) && opcao > 0 && opcao <= livrosDisponiveis.Count)
+            {
+                var livroEscolhido = livrosDisponiveis[opcao - 1];
+                return RegistrarEmprestimo(livroEscolhido);
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida.");
+                return false;
+            }
+        }
+
+
+        public bool RegistrarEmprestimo(Livros livro)
+        {
+            if (livro != null && livro.Disponivel)
+            {
+                livro.Disponivel = false;
+                Data = DateTime.Now;
+                ISBN = livro.ISBN;
+                Console.WriteLine("Empréstimo registrado com sucesso.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Livro não disponível para empréstimo.");
+                return false;
+            }
+        }
+
+    }
+}
---
+++ build/codes/model/Pendentes.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIM_3.model
+{
+    public class Pendentes
+    {
+        public int NumLivro { get; set; }
+        public int NumLeitor { get; set; }
+        public DateTime DataEmprestimo { get; set; }
+
+        public void RealizarDevolucaoPorISBN(string isbn, List<Livros> listaLivros)
+        {
+            var livro = listaLivros.FirstOrDefault(l => l.ISBN == isbn);
+
+            if (livro == null)
+            {
+                Console.WriteLine("Livro não encontrado.");
+                return;
+            }
+
+            if (!livro.Disponivel)
+            {
+                livro.Disponivel = true;
+                Console.WriteLine($"Livro \"{livro.NomeLivro}\" devolvido com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("O livro já está disponível. Nenhuma ação necessária.");
+            }
+        }
+
+
+        public void VerificarDisponibilidadeLivro(string isbn, List<Livros> listaLivros)
+        {
+            var livro = listaLivros.FirstOrDefault(l => l.ISBN == isbn);
+
+            if (livro == null)
+            {
+                Console.WriteLine("Livro não encontrado.");
+                return;
+            }
+
+            Console.WriteLine(livro.Disponivel
+                ? $"O livro \"{livro.NomeLivro}\" está disponível para empréstimo."
+                : $"O livro \"{livro.NomeLivro}\" está atualmente emprestado.");
+        }
+
+    }
+}
